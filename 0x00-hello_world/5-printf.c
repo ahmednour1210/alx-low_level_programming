eb2e8f4ac177131211dd3e3d3bf8516cb8076cd5
Ahmed Nour <ahmednour121310@gmail.com>
2023-03-16 17:57:01 -0700

    P IS capital

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
-#include <studio.h>
+#include <stdio.h>
 /**
- * main -Entry point
+ * main - Entry point
  *
- * Description: A C program that prints qith printf function.
+ * Description: A C program that prints with printf function.
  *
- * Retur always 0 (Success)
+ * Return Always 0 (Success)
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	Printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
