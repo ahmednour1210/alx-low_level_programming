cb02d4e8827670971ff98e5bffbcead8b36dfec3
Ahmed Nour <ahmednour121310@gmail.com>
2023-03-16 17:39:28 -0700

    Fixing

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,8 @@
  * Description: A C program that prints qith printf function.
  *
  * Retur always 0 (Success)
- */
-int main (void)
+*/
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
