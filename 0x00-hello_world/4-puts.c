ef66ac019b6dfc9eee53936969c62523743caa20
Ahmed Nour <ahmednour121310@gmail.com>
2023-03-16 17:28:19 -0700

    ASSEMBLER

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<studio.h>
+/**
+ * main -Entry point 
+ *
+ * Return: Alwawys 0 (Succes)
+ */
+int main(void)
+{
+	puts (""\"Programming is like building a multilingual puzzl");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+/**
+ * main -Entry point
+ *
+ * Description: A C program that prints qith printf function.
+ *
+ * Retur always 0 (Success)
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
