be2d84a9d06c8d2fea8bed0d9396b733c348163c
Ahmed Nour <ahmednour121310@gmail.com>
2023-03-17 18:02:38 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+*Description: Print alphapet lowercase.
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num=97;
+
+	while (num <= 122)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return(0);
+}
