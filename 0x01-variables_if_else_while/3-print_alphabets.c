251da013a9a6626f22e3b99afd592f96e3756a83
Ahmed Nour <ahmednour121310@gmail.com>
2023-03-17 18:18:21 -0700

    Alphapet Uppercase and lowercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Description :Pint alphapet lowercase and uppercase.
+*
+*Description: Print alphapet lowercase.
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num = 97;
+	int num2 = 65;
+
+	while (num <= 122)
+	{
+		putchar(num);
+		num++;
+	}
+	while (num2 <= 90)
+	{
+		putchar(num);
+		num2++;
+	}
+	putchar('\n');
+	return (0);
+}
