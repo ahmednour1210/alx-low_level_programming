6e597decc74c01ef7d974931ef747f6ac6db7a7b
Ahmed Nour <ahmednour121310@gmail.com>
2023-03-17 19:23:25 -0700

    print number

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,16 +12,13 @@ int main(void)
 
 	for (n = 0 ; n < 10 ; n++)
 	{
+		putchar(n + '0');
 		if (n == 9)
 		{
-			putchar(n + '0');
-		}
-		else
-		{
-		putchar(i + '0');
 		putchar(',');
 		putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
