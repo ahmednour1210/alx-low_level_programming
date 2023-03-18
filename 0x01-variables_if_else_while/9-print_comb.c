9de0e8889c86dfdd7046535f8720f88c13f7e2ab
Ahmed Nour <ahmednour121310@gmail.com>
2023-03-17 19:11:41 -0700

    print number

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+*Description: Print numbers.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 57 ; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
